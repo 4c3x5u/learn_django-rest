7f514d54d6f21776650f4d7d86c9e7f778054968
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-23 07:57:51 +0000

    3.1 ProductSerializer updated to validate the... ...description field and insert the computed fields in a cleaner way.

--- store/serializers.py
+++ store/serializers.py
@@ -4,13 +4,13 @@ from store.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    is_on_sale = serializers.BooleanField(read_only=True)
+    current_price = serializers.FloatField(read_only=True)
+
+    # field validation
+    description = serializers.CharField(min_length=2, max_length=200)
+
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'sale_start',
-                  'sale_end')
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data['is_on_sale'] = instance.is_on_sale()
-        data['current_price'] = instance.current_price()
-        return data
+                  'sale_end', 'is_on_sale', 'current_price')
